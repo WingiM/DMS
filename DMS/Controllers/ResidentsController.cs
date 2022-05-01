a3da24f212f2586801aa8548fa8edbfedbc2c043
WingiM <wingimobile@gmail.com>
2022-05-01 14:41:04 +0300

    Make PUT and POST methods of ResidentsController

--- DMS/Controllers/ResidentsController.cs
+++ DMS/Controllers/ResidentsController.cs
@@ -1,6 +1,10 @@
-﻿using DMS.Resources;
+﻿using System.Text;
+using System.Text.Json;
+using DMS.Models;
+using DMS.Resources;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace DMS.Controllers;
 
@@ -12,16 +16,96 @@ public class ResidentsController : ControllerBase
     private readonly ILogger<ResidentsController> _logger;
     private readonly ResidentResource _resource;
 
-    public ResidentsController(ILogger<ResidentsController> logger, ResidentResource resource)
+    public ResidentsController(ILogger<ResidentsController> logger,
+        ResidentResource resource)
     {
         _logger = logger;
         _resource = resource;
     }
 
     [HttpGet]
-    public IResult GetAll()
+    public IResult GetAllResidents()
     {
         return Results.Ok(_resource.GetAllResidents());
+    }
+
+    [HttpPost]
+    public async Task<IResult> AddResident()
+    {
+        Resident? resident = null;
+        StreamReader? reader = null;
+        try
+        {
+            reader = new StreamReader(Request.Body);
+            var message = await reader.ReadToEndAsync();
+            resident =
+                System.Text.Json.JsonSerializer.Deserialize<Resident>(message);
+        }
+        catch (Exception e)
+        {
+            _logger.Log(LogLevel.Information, e.ToString());
+        }
+        finally
+        {
+            reader?.Close();
+        }
+
+        if (resident is null)
+            return Results.BadRequest("Wrong JSON format");
+
+        var addResult = _resource.AddResident(resident);
+
+        if (!addResult.Item1)
+        {
+            return Results.Conflict(addResult.Item2);
+        }
+
+        return Results.Ok("Added successfully");
+    }
+
+
+    [HttpGet]
+    [Route("/api/[controller]/{id:int}")]
+    public IResult GetResidentById(int id)
+    {
+        var res = _resource.GetResidentById(id);
+        if (res is null)
+            return Results.BadRequest("Wrong id");
+
+        return Results.Ok(res);
+    }
+
+    [HttpPut]
+    [Route("/api/[controller]")]
+    public async Task<IResult> UpdateResidentInfo()
+    {
+        Resident? resident = null;
+        StreamReader? reader = null;
+        try
+        {
+            reader = new StreamReader(Request.Body);
+            var message = await reader.ReadToEndAsync();
+            resident =
+                System.Text.Json.JsonSerializer.Deserialize<Resident>(message);
+        }
+        catch (Exception e)
+        {
+            _logger.Log(LogLevel.Information, e.ToString());
+        }
+        finally
+        {
+            reader?.Close();
+        }
+
+        if (resident is null)
+            return Results.BadRequest("Wrong JSON format");
+
+        var updateResult = _resource.UpdateResident(resident);
+
+        if (!updateResult.Item1)
+            return Results.Conflict(updateResult.Item2);
+
+        return Results.Ok("Updated successfully");
     }
 
     [HttpDelete]
--- DMS/Resources/ResidentResource.cs
+++ DMS/Resources/ResidentResource.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
+using System.Diagnostics;
 using DMS.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DMS.Resources;
 
@@ -8,30 +10,81 @@ public class ResidentResource
     private readonly ApplicationContext _context;
     private readonly ILogger<ResidentResource> _logger;
 
-    public ResidentResource(ApplicationContext context, ILogger<ResidentResource> logger)
+    public ResidentResource(ApplicationContext context,
+        ILogger<ResidentResource> logger)
     {
         _context = context;
         _logger = logger;
-        _logger.Log(LogLevel.Debug, "ResidentResource!");
     }
 
     public IEnumerable GetAllResidents()
     {
-        return _context.Residents.Select(r => new
+        return _context.Residents
+            .Include(r => r.SettlementOrders)
+            .Include(r => r.SettlementOrders)
+            .Include(r => r.RatingOperations)
+            .Include(r => r.Room)
+            .AsSplitQuery();
+    }
+
+    public Resident? GetResidentById(int id)
+    {
+        return _context.Residents
+            .Include(r => r.SettlementOrders)
+            .Include(r => r.SettlementOrders)
+            .Include(r => r.RatingOperations)
+            .Include(r => r.Room)
+            .AsSplitQuery().FirstOrDefault(res => res.ResidentId == id);
+    }
+
+    public Tuple<bool, string?> AddResident(Resident resident)
+    {
+        string? errorMessage;
+        try
         {
-            r.ResidentId, r.LastName, r.FirstName, r.Patronymic, r.Gender,
-            r.PassportInformation, r.Tin, r.BirthDate
-        });
+            _context.Residents.Add(resident);
+            _context.SaveChanges();
+            return new Tuple<bool, string?>(true, null);
+        }
+        catch (DbUpdateException e)
+        {
+            _logger.Log(LogLevel.Information,
+                "Failed to insert resident:\n " + e);
+            errorMessage = (e.InnerException as Npgsql.PostgresException)
+                ?.MessageText;
+        }
+
+        return new Tuple<bool, string?>(false, errorMessage);
+    }
+
+    public Tuple<bool, string?> UpdateResident(Resident resident)
+    {
+        string? errorMessage;
+        try
+        {
+            _context.Residents.Update(resident);
+            _context.SaveChanges();
+            return new Tuple<bool, string?>(true, null);
+        }
+        catch (DbUpdateException e)
+        {
+            _logger.Log(LogLevel.Information,
+                "Failed to update resident\n " + e);
+            errorMessage = (e.InnerException as Npgsql.PostgresException)
+                ?.MessageText;
+        }
+
+        return new Tuple<bool, string?>(false, errorMessage);
     }
 
     public bool DeleteResident(int id)
     {
         var resident =
             _context.Residents.FirstOrDefault(r => r.ResidentId == id);
+
         if (resident is null)
             return false;
 
-        _logger.Log(LogLevel.Debug, "I'm working!");
         _context.Residents.Remove(resident);
         _context.SaveChanges();
         return true;
