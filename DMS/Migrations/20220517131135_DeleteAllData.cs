0199edca6aa3ffef38767f1a62c4eaf8714fb2d9
Vamsilver <vamsilver145@gmail.com>
2022-05-17 16:12:52 +0300

    Delete All DB Data

---
+++ DMS/Migrations/20220517131135_DeleteAllData.cs
@@ -0,0 +1,420 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DMS.Migrations
+{
+    public partial class DeleteAllData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "eviction_order",
+                keyColumn: "order_id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "eviction_order",
+                keyColumn: "order_id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "eviction_order",
+                keyColumn: "order_id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "eviction_order",
+                keyColumn: "order_id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "eviction_order",
+                keyColumn: "order_id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "passport_information",
+                keyColumn: "passport_information_id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "passport_information",
+                keyColumn: "passport_information_id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "passport_information",
+                keyColumn: "passport_information_id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "passport_information",
+                keyColumn: "passport_information_id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "passport_information",
+                keyColumn: "passport_information_id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "rating_operation",
+                keyColumn: "rating_operation_id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "rating_operation",
+                keyColumn: "rating_operation_id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "rating_operation",
+                keyColumn: "rating_operation_id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "rating_operation",
+                keyColumn: "rating_operation_id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "rating_operation",
+                keyColumn: "rating_operation_id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "rating_operation",
+                keyColumn: "rating_operation_id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "room",
+                keyColumn: "room_number",
+                keyValue: 308);
+
+            migrationBuilder.DeleteData(
+                table: "room",
+                keyColumn: "room_number",
+                keyValue: 309);
+
+            migrationBuilder.DeleteData(
+                table: "settlement_order",
+                keyColumn: "order_id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "settlement_order",
+                keyColumn: "order_id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "settlement_order",
+                keyColumn: "order_id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "settlement_order",
+                keyColumn: "order_id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "settlement_order",
+                keyColumn: "order_id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "settlement_order",
+                keyColumn: "order_id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "settlement_order",
+                keyColumn: "order_id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "settlement_order",
+                keyColumn: "order_id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "settlement_order",
+                keyColumn: "order_id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "settlement_order",
+                keyColumn: "order_id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "settlement_order",
+                keyColumn: "order_id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "transaction",
+                keyColumn: "transaction_id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "transaction",
+                keyColumn: "transaction_id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "transaction",
+                keyColumn: "transaction_id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "transaction",
+                keyColumn: "transaction_id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "transaction",
+                keyColumn: "transaction_id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "transaction",
+                keyColumn: "transaction_id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "rating_change_category",
+                keyColumn: "category_id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "rating_change_category",
+                keyColumn: "category_id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "rating_change_category",
+                keyColumn: "category_id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "resident",
+                keyColumn: "resident_id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "resident",
+                keyColumn: "resident_id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "resident",
+                keyColumn: "resident_id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "resident",
+                keyColumn: "resident_id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "resident",
+                keyColumn: "resident_id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "resident",
+                keyColumn: "resident_id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "resident",
+                keyColumn: "resident_id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "resident",
+                keyColumn: "resident_id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "resident",
+                keyColumn: "resident_id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "resident",
+                keyColumn: "resident_id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "resident",
+                keyColumn: "resident_id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "room",
+                keyColumn: "room_number",
+                keyValue: 301);
+
+            migrationBuilder.DeleteData(
+                table: "room",
+                keyColumn: "room_number",
+                keyValue: 302);
+
+            migrationBuilder.DeleteData(
+                table: "room",
+                keyColumn: "room_number",
+                keyValue: 303);
+
+            migrationBuilder.DeleteData(
+                table: "room",
+                keyColumn: "room_number",
+                keyValue: 304);
+
+            migrationBuilder.DeleteData(
+                table: "room",
+                keyColumn: "room_number",
+                keyValue: 305);
+
+            migrationBuilder.DeleteData(
+                table: "room",
+                keyColumn: "room_number",
+                keyValue: 306);
+
+            migrationBuilder.DeleteData(
+                table: "room",
+                keyColumn: "room_number",
+                keyValue: 307);
+
+            migrationBuilder.DeleteData(
+                table: "room",
+                keyColumn: "room_number",
+                keyValue: 407);
+
+            migrationBuilder.DeleteData(
+                table: "room",
+                keyColumn: "room_number",
+                keyValue: 423);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "rating_change_category",
+                columns: new[] { "category_id", "name" },
+                values: new object[,]
+                {
+                    { 1, "Докладная" },
+                    { 2, "Поощрение" },
+                    { 3, "Выговор" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "room",
+                columns: new[] { "room_number", "capacity", "gender" },
+                values: new object[,]
+                {
+                    { 301, 3, 'M' },
+                    { 302, 3, 'M' },
+                    { 303, 3, 'M' },
+                    { 304, 3, 'M' },
+                    { 305, 3, 'M' },
+                    { 306, 3, 'M' },
+                    { 307, 3, 'M' },
+                    { 308, 3, 'M' },
+                    { 309, 3, 'M' },
+                    { 407, 3, 'M' },
+                    { 423, 3, 'M' }
+                });
+
+            migrationBuilder.InsertData(
+                table: "resident",
+                columns: new[] { "resident_id", "birth_date", "course", "first_name", "gender", "is_commercial", "last_name", "patronymic", "room_number", "tin" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4543), null, "Даниил", 'M', false, "Морозов", "Артёмович", 301, null },
+                    { 2, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4546), null, "Роман", 'M', false, "Васильев", "Алексеевич", 423, null },
+                    { 3, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4547), null, "Doma", 'M', false, "Vasilev", "Артёмович", 301, null },
+                    { 4, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4549), null, "Goma", 'M', false, "Vasilev", "Артёмович", 301, null },
+                    { 5, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4551), null, "Doma", 'M', false, "Vasilev", "Артёмович", 302, null },
+                    { 6, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4553), null, "Moma", 'M', false, "Vasilev", "Артёмович", 303, null },
+                    { 7, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4555), null, "Noma", 'M', false, "Vasilev", "Артёмович", 304, null },
+                    { 8, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4557), null, "Bulat", 'M', false, "Vasilev", "Артёмович", 305, null },
+                    { 9, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4559), null, "Bulad", 'M', false, "Vasilev", "Артёмович", 306, null },
+                    { 10, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4561), null, "Bular", 'M', false, "Vasilev", "Артёмович", 307, null },
+                    { 11, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4563), null, "Bulas", 'M', false, "Vasilev", "Артёмович", 407, null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "eviction_order",
+                columns: new[] { "order_id", "description", "order_date", "resident_id" },
+                values: new object[,]
+                {
+                    { 1, null, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4944), 11 },
+                    { 2, null, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4945), 10 },
+                    { 3, null, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4946), 9 },
+                    { 4, null, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4947), 8 },
+                    { 5, null, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4948), 7 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "passport_information",
+                columns: new[] { "passport_information_id", "address", "department_code", "issue_date", "issued_by", "resident_id", "series_number" },
+                values: new object[,]
+                {
+                    { 1, "asdasdasdas", 23124, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(5105), "МВД по чему-то", 1, "1234567890" },
+                    { 2, "sadgsdfgfdg", 23124, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(5109), "МВД по чему-то", 2, "1523123123" },
+                    { 3, "sdghgfhgfhfgdh", 23423, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(5110), "МВД по чему-то", 3, "7916239123" },
+                    { 4, "asdfsdfdsaf", 54334, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(5111), "МВД по чему-то", 4, "9817349817" },
+                    { 5, "gdfsgfdsgdsf", 98172, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(5113), "МВД по чему-то", 5, "1807231987" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "rating_operation",
+                columns: new[] { "rating_operation_id", "category_id", "change_value", "description", "order_date", "resident_id" },
+                values: new object[,]
+                {
+                    { 1, 1, -3, null, new DateTime(2021, 10, 8, 21, 0, 0, 0, DateTimeKind.Utc), 1 },
+                    { 2, 2, 2, null, new DateTime(2021, 10, 13, 21, 0, 0, 0, DateTimeKind.Utc), 6 },
+                    { 3, 2, 2, null, new DateTime(2021, 10, 12, 21, 0, 0, 0, DateTimeKind.Utc), 5 },
+                    { 4, 1, -2, null, new DateTime(2021, 10, 11, 21, 0, 0, 0, DateTimeKind.Utc), 4 },
+                    { 5, 3, -1, null, new DateTime(2021, 10, 10, 21, 0, 0, 0, DateTimeKind.Utc), 3 },
+                    { 6, 2, 2, null, new DateTime(2021, 10, 9, 21, 0, 0, 0, DateTimeKind.Utc), 2 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "settlement_order",
+                columns: new[] { "order_id", "description", "order_date", "parents_name", "parents_passport_address", "parents_passport_department_code", "parents_passport_issue_date", "parents_passport_issued_by", "parents_passport_series_number", "resident_id", "room_number" },
+                values: new object[,]
+                {
+                    { 1, null, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4912), null, null, null, null, null, null, 1, 301 },
+                    { 2, null, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4914), null, null, null, null, null, null, 2, 423 },
+                    { 3, null, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4915), null, null, null, null, null, null, 3, 301 },
+                    { 4, null, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4916), null, null, null, null, null, null, 4, 301 },
+                    { 5, null, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4917), null, null, null, null, null, null, 5, 302 },
+                    { 6, null, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4918), null, null, null, null, null, null, 6, 303 },
+                    { 7, null, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4920), null, null, null, null, null, null, 7, 304 },
+                    { 8, null, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4921), null, null, null, null, null, null, 8, 305 },
+                    { 9, null, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4922), null, null, null, null, null, null, 9, 306 },
+                    { 10, null, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4922), null, null, null, null, null, null, 10, 307 },
+                    { 11, null, new DateTime(2022, 5, 15, 19, 5, 23, 256, DateTimeKind.Utc).AddTicks(4923), null, null, null, null, null, null, 11, 407 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "transaction",
+                columns: new[] { "transaction_id", "operation_date", "resident_id", "sum" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2021, 10, 8, 21, 0, 0, 0, DateTimeKind.Utc), 1, 3000.0 },
+                    { 2, new DateTime(2021, 10, 9, 7, 0, 0, 0, DateTimeKind.Utc), 2, 1000.0 },
+                    { 3, new DateTime(2021, 10, 9, 9, 0, 0, 0, DateTimeKind.Utc), 2, 3000.0 },
+                    { 4, new DateTime(2021, 10, 11, 21, 0, 0, 0, DateTimeKind.Utc), 3, 4000.0 },
+                    { 5, new DateTime(2021, 10, 9, 21, 0, 0, 0, DateTimeKind.Utc), 4, 2012.4200000000001 },
+                    { 6, new DateTime(2021, 10, 19, 21, 0, 0, 0, DateTimeKind.Utc), 5, 18234.130000000001 }
+                });
+        }
+    }
+}
